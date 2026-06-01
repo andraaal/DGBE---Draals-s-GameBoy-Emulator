7ae4ef548df56225ca5e797642a513c96eb8ba92
andi-coding <a.muth@gmx.at>
2026-06-02 00:57:33 +0200

    Finish MBC1 implementation

--- gameboy-emu/src/mbc/mbc1.rs
+++ gameboy-emu/src/mbc/mbc1.rs
@@ -4,8 +4,13 @@ use crate::memory::MemoryError;
 pub(crate) struct MBC1 {
     rom: Vec<[u8; 0x4000]>,
     ram: Vec<[u8; 0x2000]>,
-    rom_bank: usize,
+    rom_bank1: usize,
+    rom_bank2: usize,
     ram_bank: usize,
+    ram_enabled: bool,
+    rom_register: u8,
+    ram_register: u8,
+    advanced_banking_mode: bool,
 }
 
 impl MBC1 {
@@ -23,29 +28,52 @@ impl MBC1 {
                 ram
             ));
         }
+        let initial_rom = rom
+            .chunks(0x4000)
+            .map(|chunk| {
+                let mut bank = [0u8; 0x4000];
+                bank[..chunk.len()].copy_from_slice(chunk);
+                bank
+            })
+            .collect::<Vec<[u8; 0x4000]>>();
 
-        #[expect(unreachable_code)]
+        let mut initial_ram = Vec::new();
+        initial_ram.resize_with(ram, || [0u8; 0x2000]);
+
         Ok(Self {
-            rom: Vec::from(
-                rom.chunks(0x4000)
-                    .map(|chunk| {
-                        let mut bank = [0u8; 0x4000];
-                        bank[..chunk.len()].copy_from_slice(chunk);
-                        bank
-                    })
-                    .collect::<Vec<[u8; 0x4000]>>(),
-            ),
-            ram: todo!(),
-            rom_bank: 1,
+            rom: initial_rom,
+            ram: initial_ram,
+            rom_bank1: 0,
+            rom_bank2: 1,
             ram_bank: 0,
+            ram_enabled: false,
+            rom_register: 0x1,
+            ram_register: 0x0,
+            advanced_banking_mode: false,
         })
     }
 
     pub fn read(&self, address: u16) -> u8 {
         match address {
-            0x0000..=0x3FFF => self.rom[0][address as usize],
-            0x4000..=0x7FFF => self.rom[1][(address - 0x4000) as usize],
-            0xA000..=0xBFFF => self.ram[0][(address - 0xA000) as usize],
+            0x0000..=0x3FFF => {
+                if self.advanced_banking_mode {
+                    self.rom[self.rom_bank1][address as usize]
+                } else {
+                    self.rom[0][address as usize]
+                }
+            }
+            0x4000..=0x7FFF => self.rom[self.rom_bank2][(address - 0x4000) as usize],
+            0xA000..=0xBFFF => {
+                if self.ram_enabled {
+                    if self.advanced_banking_mode {
+                        self.ram[self.ram_bank][(address - 0xA000) as usize]
+                    } else {
+                        self.ram[0][(address - 0xA000) as usize]
+                    }
+                } else {
+                    0xFF
+                }
+            }
             _ => panic!(
                 "Tried to read address {:04X} on cartridge: This address is found on the main gameboy",
                 address
@@ -55,6 +83,34 @@ impl MBC1 {
 
     pub fn write(&mut self, address: u16, value: u8) {
         match address {
+            0x0000..=0x1FFF => self.ram_enabled = value & 0x0F == 0x0A,
+            0x2000..=0x3FFF => {
+                self.rom_register = value & 0x1F;
+                if self.rom_register == 0 {
+                    self.rom_register = 1;
+                }
+
+                let full_address = self.rom_register | (self.ram_register << 5);
+                self.rom_bank2 = (full_address & (self.rom.len() as u8 - 1)) as usize;
+            }
+            0x4000..=0x5FFF => {
+                self.ram_register = value & 0x03;
+                if self.advanced_banking_mode {
+                    if self.ram.len() > 1 {
+                        self.ram_bank = (self.ram_register & (self.ram.len() as u8 - 1)) as usize;
+                    } else {
+                        self.rom_bank2 = (self.ram_register << 5) as usize;
+                    }
+                    let full_address = self.rom_register | (self.ram_register << 5);
+                    self.rom_bank2 = (full_address & (self.rom.len() as u8 - 1)) as usize;
+                } else {
+                    let full_address = self.rom_register | (self.ram_register << 5);
+                    self.rom_bank2 = (full_address & (self.rom.len() as u8 - 1)) as usize;
+                }
+            }
+            0x6000..=0x7FFF => {
+                self.advanced_banking_mode = value & 0x01 == 0x01;
+            }
             0xA000..=0xBFFF => self.ram[0][(address - 0xA000) as usize] = value,
             _ => panic!(
                 "Tried to write to address {:04X} on cartridge: This address is found on the main gameboy",
